a6f6059f66accd7d949a635f4c91ed1a6e1e29be
obidkor <coavldjs93@naver.com>
2021-08-02 23:42:46 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
-import express, { application } from "express";
-
+import express from "express";
 
 const app = express();
-console.log("hello");
 
-app.listen(3000);
+app.set("view engine","pug");  //view engine
+app.set("views", __dirname + "/views"); // template
+app.use("/public", express.static(__dirname + "/public"));  // public url(share file with users)
+app.get("/", (req,res) => res.render("home")); // only route handler =>> rendering home.pug
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
