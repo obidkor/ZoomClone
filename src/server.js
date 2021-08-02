ecc76d7d67373bbc8c8f8d1de37d15adddf3ee6a
obidkor <coavldjs93@naver.com>
2021-08-02 23:50:41 +0900

    add force to redirect home

--- src/server.js
+++ src/server.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.set("view engine","pug");  //view engine
 app.set("views", __dirname + "/views"); // template
-app.use("/public", express.static(__dirname + "/public"));  // public url(share file with users)
-app.get("/", (req,res) => res.render("home")); // only route handler =>> rendering home.pug
+app.use("/public", express.static(__dirname + "/public"));  // public url(share file with users) which is the frontend file
+app.get("/", (req,res) => res.render("home")); // only route handler =>> rendering home.
+app.get("/*", (req,res) => res.redirect("/")); // force to backward to home
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
