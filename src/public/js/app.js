9c9cfddac63d112e751b83cfa8b623dcd17b9a4a
obidkor <coavldjs93@naver.com>
2021-08-19 00:18:25 +0900

    websocket: just connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,2 @@
-alert("hi!");
+// 프론트 소켓 객체... 이걸로 이벤트 주고받음.
+const socket = new WebSocket(`ws://${window.location.host}`);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 
-app.set("view engine","pug");  //view engine
+app.set("view engine", "pug"); //view engine
 app.set("views", __dirname + "/views"); // template
-app.use("/public", express.static(__dirname + "/public"));  // public url(share file with users) which is the frontend file
-app.get("/", (req,res) => res.render("home")); // only route handler =>> rendering home.
-app.get("/*", (req,res) => res.redirect("/")); // force to backward to home
+app.use("/public", express.static(__dirname + "/public")); // public url(share file with users) which is the frontend file
+app.get("/", (req, res) => res.render("home")); // only route handler =>> rendering home.
+app.get("/*", (req, res) => res.redirect("/")); // force to backward to home
 
+// http protocol
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+// host
+const server = http.createServer(app);
+// websocket server in http server
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+  console.log(socket);
+}
+
+wss.on("connection", handleConnection);
+
+// port
+server.listen(3000, handleListen);
