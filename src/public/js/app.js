b0994c4e51d76badcdc48f15846be8a375bb3a27
obidkor <coavldjs93@naver.com>
2022-10-18 16:24:49 +0900

    camera/audio setting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,78 @@
+// socket io version 시작
+// 백엔드 socket io와 이벤트 주고받는 펑션
+const socket = io();
+
+//video 화면
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
+// audio + video = stream / settings
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      cameraSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+//user의 userMedia string
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+    await getCameras();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Media 켜두기
+getMedia();
+
+// Audio Tracks 가지고 와서 enable
+function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "UnMute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+// Video Tracks 가지고 와서 enable
+function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
 // 브라우저 내장 라이브러리 WebSocket으로 만든것 시작
 // 프론트 소켓 객체... 이걸로 이벤트 주고받음.
 //const socket = new WebSocket(`ws://${window.location.host}`); // 브라우저 내장 소켓라이브러리
@@ -29,93 +104,91 @@
 // }
 // 브라우저 내장 라이브러리 websocket으로 만든것 끝
 
-// socket io version 시작
-// 백엔드 socket io와 이벤트 주고받는 펑션
-const socket = io();
+// 2022-10-18, socketio chat프로그래 주석시작
+// const welcome = document.getElementById("welcome");
+// const form = welcome.querySelector("form");
+// const room = document.getElementById("room");
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+// room.hidden = true;
 
-room.hidden = true;
+// let roomName;
 
-let roomName;
+// function handleMessageSubmit(event) {
+//   event.preventDefault();
+//   const input = room.querySelector("#msg input");
+//   const value = input.value;
+//   socket.emit("new_message", value, roomName, () => {
+//     addMessage(`You: ${value}`);
+//   });
+//   input.value = "";
+// }
 
-function handleMessageSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new_message", value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
-}
+// function handleNicknameSubmit(event) {
+//   event.preventDefault();
+//   const input = room.querySelector("#name input");
+//   socket.emit("nickname", input.value);
+// }
 
-function handleNicknameSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#name input");
-  socket.emit("nickname", input.value);
-}
+// function showRoom() {
+//   welcome.hidden = true;
+//   room.hidden = false;
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room : ${roomName}`;
+//   const msgForm = room.querySelector("#msg");
+//   const nameForm = room.querySelector("#name");
+//   msgForm.addEventListener("submit", handleMessageSubmit);
+//   nameForm.addEventListener("submit", handleNicknameSubmit);
+// }
 
-function showRoom() {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room : ${roomName}`;
-  const msgForm = room.querySelector("#msg");
-  const nameForm = room.querySelector("#name");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-  nameForm.addEventListener("submit", handleNicknameSubmit);
-}
+// function addMessage(msg) {
+//   const ul = room.querySelector("ul");
+//   const li = document.createElement("li");
+//   li.innerText = msg;
+//   ul.appendChild(li);
+// }
 
-function addMessage(msg) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = msg;
-  ul.appendChild(li);
-}
+// function handleRoomSubmit(event) {
+//   event.preventDefault();
+//   const input = form.querySelector("input");
 
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
+//   // websocket의 socketsend // room이라는 event를 emit
+//   // websocket은 이벤트명이정해져있지만 socketio는 이벤트명을 정할수 이씀
+//   // websocket은 string을 보내지만 socketio는 object를 전송할수 있다.
+//   // 3번째 args로 fuction을 보내고 있음.(callback 용도? back에서 보내진 fuc을 호출할 수 있음.. 실행은 front에서 됨. callback fucc은 마지막 args여야함..)
+//   // agrs의 숫자는 원하는 만큼 보낼수 있는듯?
+//   socket.emit("enter_room", input.value, showRoom);
+//   roomName = input.value;
+//   input.value = "";
+// }
 
-  // websocket의 socketsend // room이라는 event를 emit
-  // websocket은 이벤트명이정해져있지만 socketio는 이벤트명을 정할수 이씀
-  // websocket은 string을 보내지만 socketio는 object를 전송할수 있다.
-  // 3번째 args로 fuction을 보내고 있음.(callback 용도? back에서 보내진 fuc을 호출할 수 있음.. 실행은 front에서 됨. callback fucc은 마지막 args여야함..)
-  // agrs의 숫자는 원하는 만큼 보낼수 있는듯?
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
-}
+// form.addEventListener("submit", handleRoomSubmit);
 
-form.addEventListener("submit", handleRoomSubmit);
+// socket.on("welcome", (nickname, newCount) => {
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room : ${roomName} (${newCount})`;
+//   addMessage(`${nickname} joined`);
+// });
 
-socket.on("welcome", (nickname, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room : ${roomName} (${newCount})`;
-  addMessage(`${nickname} joined`);
-});
+// socket.on("bye", (nickname, newCount) => {
+//   const h3 = room.querySelector("h3");
+//   h3.innerText = `Room : ${roomName} (${newCount})`;
+//   addMessage(`${nickname} left ㅠㅠ`);
+// });
 
-socket.on("bye", (nickname, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room : ${roomName} (${newCount})`;
-  addMessage(`${nickname} left ㅠㅠ`);
-});
+// //socket.on("new_message", (msg) => {addmessage(msg)});
+// socket.on("new_message", addMessage);
 
-//socket.on("new_message", (msg) => {addmessage(msg)});
-socket.on("new_message", addMessage);
-
-// room count event
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if (rooms.length === 0) {
-    return;
-  }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.append(li);
-  });
-});
+// // room count event
+// socket.on("room_change", (rooms) => {
+//   const roomList = welcome.querySelector("ul");
+//   roomList.innerHTML = "";
+//   if (rooms.length === 0) {
+//     return;
+//   }
+//   rooms.forEach((room) => {
+//     const li = document.createElement("li");
+//     li.innerText = room;
+//     roomList.append(li);
+//   });
+// });
+// 2022-10-18, socketio chat프로그래 주석끝
--- src/server.js
+++ src/server.js
@@ -36,89 +36,92 @@ instrument(wsSever, {
 //   },
 // });
 
-// publicroom만 리턴하는 펑션(defalut socket ID != room socket ID 이면 public room)
-function publicRooms() {
-  //const sid = wsSever.sockets.adapter.sids;
-  //const room = wsSever.sockets.adapter.rooms;
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsSever;
-
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-
-  return publicRooms;
-}
-
-// Room에 조인된 sid를 카운트하는 펑션
-function countRoom(roomName) {
-  return wsSever.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-//connection
-wsSever.on("connection", (socket) => {
-  // socket io 구독 이벤트는 여기에 작성해준다.
-
-  socket["nickname"] = "Anon";
-
-  // onAny : 모든 이벤트에서 공통으로 실행
-  socket.onAny((events) => {
-    console.log(`Socket events : ${events}`);
-  });
-
-  socket.on("enter_room", (roomName, showRoom) => {
-    //최초 room은 connection되면 자동으로 생김.
-    //console.log(socket.id); // socket.id : 현재 그룹 id
-    //console.log(`beform join:`, socket.rooms); // socket.rooms그룹이 어떤게 있는가
-
-    // socket.join : socket끼리 그룹짓기(room 개념) : join하면 기존에 없으면 새로생기고 있는거면 거기에 그룹화됨.
-    // join(1,2,3,4) : 여러개 방에 동시에 입장도 가능
-    socket.join(roomName);
-    showRoom();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // roomname에 들어있는 모든 socketid에 noti
-    //socket.leave(string roomname) : 방 떠나기
-    //socket.to(string roomname).emit(이벤트명) : 방전체에 이벤트 생성하기 chaining 이라 to().to()...이런식으로 가능 ==> 나를 제외한 room사람들에게 broadcast
-    //socket.to(socketid).emit(이벤트): private 이벤트를 보낼수도 있음.
-
-    // 모든 socket에 publicRoom 리스트 전송
-    wsSever.sockets.emit("room_change", publicRooms());
-  });
-
-  // disconnecting은 socketio 내장 이벤트명이다..
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => {
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
-    });
-  });
-
-  socket.on("disconnect", () => {
-    // 모든 socket에 publicRoom 리스트 전송
-    wsSever.sockets.emit("room_change", publicRooms());
-  });
-
-  // message보내는 이벤트
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done(); //화면에 appendchild
-  });
-
-  //nickname save 이벤트
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname)); // socket에 닉네임 저장
-});
-
 // http protocol
 const handleListen = () => console.log(`Listening on http://localhost:4000`);
 // port
 // app.listen(3000, handleListen);
 httpServer.listen(4000, handleListen);
 
-// 2022-10-12, socketio 임포트를 위해 주석처리
+// 2022-10-18, socketio chat프로그램 주석처리 시작
+// // publicroom만 리턴하는 펑션(defalut socket ID != room socket ID 이면 public room)
+// function publicRooms() {
+//   //const sid = wsSever.sockets.adapter.sids;
+//   //const room = wsSever.sockets.adapter.rooms;
+//   const {
+//     sockets: {
+//       adapter: { sids, rooms },
+//     },
+//   } = wsSever;
+
+//   const publicRooms = [];
+//   rooms.forEach((_, key) => {
+//     if (sids.get(key) === undefined) {
+//       publicRooms.push(key);
+//     }
+//   });
+
+//   return publicRooms;
+// }
+
+// // Room에 조인된 sid를 카운트하는 펑션
+// function countRoom(roomName) {
+//   return wsSever.sockets.adapter.rooms.get(roomName)?.size;
+// }
+
+// //connection
+// wsSever.on("connection", (socket) => {
+//   // socket io 구독 이벤트는 여기에 작성해준다.
+
+//   socket["nickname"] = "Anon";
+
+//   // onAny : 모든 이벤트에서 공통으로 실행
+//   socket.onAny((events) => {
+//     console.log(`Socket events : ${events}`);
+//   });
+
+//   socket.on("enter_room", (roomName, showRoom) => {
+//     //최초 room은 connection되면 자동으로 생김.
+//     //console.log(socket.id); // socket.id : 현재 그룹 id
+//     //console.log(`beform join:`, socket.rooms); // socket.rooms그룹이 어떤게 있는가
+
+//     // socket.join : socket끼리 그룹짓기(room 개념) : join하면 기존에 없으면 새로생기고 있는거면 거기에 그룹화됨.
+//     // join(1,2,3,4) : 여러개 방에 동시에 입장도 가능
+//     socket.join(roomName);
+//     showRoom();
+//     socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // roomname에 들어있는 모든 socketid에 noti
+//     //socket.leave(string roomname) : 방 떠나기
+//     //socket.to(string roomname).emit(이벤트명) : 방전체에 이벤트 생성하기 chaining 이라 to().to()...이런식으로 가능 ==> 나를 제외한 room사람들에게 broadcast
+//     //socket.to(socketid).emit(이벤트): private 이벤트를 보낼수도 있음.
+
+//     // 모든 socket에 publicRoom 리스트 전송
+//     wsSever.sockets.emit("room_change", publicRooms());
+//   });
+
+//   // disconnecting은 socketio 내장 이벤트명이다..
+//   socket.on("disconnecting", () => {
+//     socket.rooms.forEach((room) => {
+//       socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
+//     });
+//   });
+
+//   socket.on("disconnect", () => {
+//     // 모든 socket에 publicRoom 리스트 전송
+//     wsSever.sockets.emit("room_change", publicRooms());
+//   });
+
+//   // message보내는 이벤트
+//   socket.on("new_message", (msg, room, done) => {
+//     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+//     done(); //화면에 appendchild
+//   });
+
+//   //nickname save 이벤트
+//   socket.on("nickname", (nickname) => (socket["nickname"] = nickname)); // socket에 닉네임 저장
+// });
+
+// 2022-10-18, socketio chat프로그램 주석처리 끝
+
+// 2022-10-12, socketio 임포트를 위해 주석처리 시작
 
 // // websocket server in http server
 //// host
@@ -158,7 +161,7 @@ httpServer.listen(4000, handleListen);
 //     }
 //   });
 // });
-
+// 2022-10-12, socketio 임포트를 위해 주석처리 끝
 //git 명령어
 // git add .
 // git commit -m "comment"
